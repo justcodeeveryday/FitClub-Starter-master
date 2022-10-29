0eefc4250c724dc9eb5d5cf6d21f681f9ed07c2f
Stojan <stoyan97@gmail.com>
2022-10-29 12:15:09 +0200

    first commit

---
+++ src/components/Join/Join.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useRef } from "react";
+import './Join.css';
+import emailjs from '@emailjs/browser';
+
+const Join = () => {
+    const form = useRef()
+    return (
+        <div className="Join" id='join-us'>
+        <div className="left-j">
+        <hr/>
+        <div>
+            <span className="stroke-text">READY TO</span>
+            <span> LEVEL UP</span>
+        </div>
+        <div>
+            <span>YOUR BODY</span>
+            <span className="stroke-text"> WITH US?</span>
+        </div>
+        </div>
+        <div className="right-j">
+            <form ref={form} action="" className="email-container">
+                <input type='email' name="user_email" placeholder="Enter your e-mail adress"/>
+                <button className="btn btn-j">Join now</button>
+            
+            </form>
+        </div>
+        </div>
+    )
+}
+export default Join;
